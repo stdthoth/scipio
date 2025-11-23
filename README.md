e51485d731a2c337429eccd555b67bfce91bff7d
Shalom Tata <ichthoth@gmail.com>
2025-11-23 17:07:48 +0100

    small fix on previousl created package

--- README.md
+++
@@ -1,1 +0,0 @@
-experimental package for audio processing and signal processing
---
+++ _obj/scipio.cgo2.c
@@ -0,0 +1,599 @@
+
+#line 1 "cgo-builtin-prolog"
+#include <stddef.h>
+
+/* Define intgo when compiling with GCC.  */
+typedef ptrdiff_t intgo;
+
+#define GO_CGO_GOSTRING_TYPEDEF
+typedef struct { const char *p; intgo n; } _GoString_;
+typedef struct { char *p; intgo n; intgo c; } _GoBytes_;
+_GoString_ GoString(char *p);
+_GoString_ GoStringN(char *p, int l);
+_GoBytes_ GoBytes(void *p, int n);
+char *CString(_GoString_);
+void *CBytes(_GoBytes_);
+void *_CMalloc(size_t);
+
+__attribute__ ((unused))
+static size_t _GoStringLen(_GoString_ s) { return (size_t)s.n; }
+
+__attribute__ ((unused))
+static const char *_GoStringPtr(_GoString_ s) { return s.p; }
+
+#line 3 "/home/thoth/Developer/scipio/scipio.go"
+
+ #include <stdlib.h>
+ #include <sndfile.h>
+
+#line 1 "cgo-generated-wrapper"
+
+
+#line 1 "cgo-gcc-prolog"
+/*
+  If x and y are not equal, the type will be invalid
+  (have a negative array count) and an inscrutable error will come
+  out of the compiler and hopefully mention "name".
+*/
+#define __cgo_compile_assert_eq(x, y, name) typedef char name[(x-y)*(x-y)*-2UL+1UL];
+
+/* Check at compile time that the sizes we use match our expectations. */
+#define __cgo_size_assert(t, n) __cgo_compile_assert_eq(sizeof(t), (size_t)n, _cgo_sizeof_##t##_is_not_##n)
+
+__cgo_size_assert(char, 1)
+__cgo_size_assert(short, 2)
+__cgo_size_assert(int, 4)
+typedef long long __cgo_long_long;
+__cgo_size_assert(__cgo_long_long, 8)
+__cgo_size_assert(float, 4)
+__cgo_size_assert(double, 8)
+
+extern char* _cgo_topofstack(void);
+
+/*
+  We use packed structs, but they are always aligned.
+  The pragmas and address-of-packed-member are only recognized as warning
+  groups in clang 4.0+, so ignore unknown pragmas first.
+*/
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wpragmas"
+#pragma GCC diagnostic ignored "-Waddress-of-packed-member"
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#pragma GCC diagnostic ignored "-Wunaligned-access"
+
+#include <errno.h>
+#include <string.h>
+
+
+#define CGO_NO_SANITIZE_THREAD
+#define _cgo_tsan_acquire()
+#define _cgo_tsan_release()
+
+
+#define _cgo_msan_write(addr, sz)
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_free(void *v)
+{
+	struct {
+		void* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	free(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_close(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int r;
+		char __pad12[4];
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_close(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_error_number(void *v)
+{
+	struct {
+		int p0;
+		char __pad4[4];
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) sf_error_number(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_format_check(void *v)
+{
+	struct {
+		SF_INFO const* p0;
+		int r;
+		char __pad12[4];
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_format_check(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_get_string(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int p1;
+		char __pad12[4];
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) sf_get_string(_cgo_a->p0, _cgo_a->p1);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_open(void *v)
+{
+	struct {
+		char const* p0;
+		int p1;
+		char __pad12[4];
+		SF_INFO* p2;
+		SNDFILE* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) sf_open(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_open_fd(void *v)
+{
+	struct {
+		int p0;
+		int p1;
+		SF_INFO* p2;
+		int p3;
+		char __pad20[4];
+		SNDFILE* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) sf_open_fd(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2, _cgo_a->p3);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_read_double(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		double* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_read_double(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_read_float(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		float* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_read_float(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_read_int(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_read_int(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_read_short(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		short int* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_read_short(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_readf_double(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		double* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_readf_double(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_readf_float(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		float* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_readf_float(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_readf_int(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_readf_int(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_readf_short(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		short int* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_readf_short(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_seek(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		sf_count_t p1;
+		int p2;
+		char __pad20[4];
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_seek(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_set_string(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int p1;
+		char __pad12[4];
+		char const* p2;
+		int r;
+		char __pad28[4];
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_set_string(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_strerror(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) sf_strerror(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_write_double(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		double const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_write_double(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_write_float(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		float const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_write_float(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_write_int(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_write_int(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_write_short(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		short int const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_write_short(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_write_sync(void *v)
+{
+	struct {
+		SNDFILE* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	sf_write_sync(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_writef_double(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		double const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_writef_double(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_writef_float(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		float const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_writef_float(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_writef_int(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		int const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_writef_int(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_75cf4c0ad46f_Cfunc_sf_writef_short(void *v)
+{
+	struct {
+		SNDFILE* p0;
+		short int const* p1;
+		sf_count_t p2;
+		sf_count_t r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = sf_writef_short(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
